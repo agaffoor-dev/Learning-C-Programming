6bc9f419e567de3fe35c1f8769178409624a5fab
agaffoor-dev <174390644+agaffoor-dev@users.noreply.github.com>
2024-07-29 19:54:36 -0400

    Added exercise iterations for C book

---
+++ temperature_converter/converter_1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int lower, upper, fahr, cel, adding; 
+
+lower = 0;
+
+upper = 100;
+
+fahr = 0;
+
+adding = 20;
+
+while (fahr <= upper) {
+  (fahr*5)/9=cel;
+  printf("%d\t%d", fahr, cel);
+  fahr = fahr + adding;
+}
